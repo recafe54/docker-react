2ba6fb7a53d3499740e3e4bc46aa4ac7f8cc9c4c
HieuNg <phustock97@gmail.com>
2022-01-13 12:20:34 +0700

    try me!

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed
+          From Github to EBS
         </p>
         <a
           className="App-link"
